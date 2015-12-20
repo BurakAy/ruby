fab8a80097b410c839cfce33c5a79c8ec2fbebd9
BurakAy <burak@burak-aydemir.com>
2015-12-20 15:11:57 -0600

    update english number method'

--- yourownmethods.rb
+++ yourownmethods.rb
@@ -79,6 +79,8 @@ puts
 puts nosePick
 =end
 
+
+=begin
 def englishNumber number
 	if number < 0
 		return 'Please enter a number zero or greater.'
@@ -201,3 +203,79 @@ puts englishNumber(83)
 puts englishNumber(95)
 puts englishNumber(70)
 puts englishNumber(100)
+=end
+
+# Clean up above method with arrays
+def englishNumber number
+	if number < 0 # No negatives
+		return 'Please enter a number that isn\'t negative'
+	end
+
+	if number == 0
+		return 'zero'
+	end
+
+	numString = '' # string to be returned
+
+	onesPlace = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+	tensPlace = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+	teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+	# 'left' is how much of the number we still have left to write out
+	# 'write' is the part we are currently writing out
+	left = number
+	write = left/100
+	left = left - write * 100
+
+	if write > 0
+		hundreds = englishNumber write # recursion, call itself
+		numString = numString + hundreds + ' hundred'
+
+		if left > 0
+			numString = numString + ' '
+		end
+	end
+
+	write = left/10
+	left = left - write * 10
+
+	if write > 0
+		if ((write == 1) and (left > 0))
+			numString = numString + teens[left - 1]
+			left = 0
+		else
+			numString = numString + tensPlace[write - 1]
+		end
+
+		if left > 0
+			numString = numString + '-'
+		end
+	end
+
+	write = left
+	left = 0
+
+	if write > 0
+		numString = numString + onesPlace[write - 1]
+	end
+
+	numString
+end
+
+puts englishNumber(1)
+puts englishNumber(59)
+puts englishNumber(44)
+puts englishNumber(36)
+puts englishNumber(18)
+puts englishNumber(22)
+puts englishNumber(0)
+puts englishNumber(67)
+puts englishNumber(83)
+puts englishNumber(95)
+puts englishNumber(70)
+puts englishNumber(100)
+puts englishNumber(101)
+puts englishNumber(234)
+puts englishNumber(3211)
+puts englishNumber(999999)
+puts englishNumber(1000000000000)
